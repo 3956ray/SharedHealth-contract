4c17b82d188a3e3ae1a6070e2baa782543108262
3956ray <3956ray@gmail.com>
2022-04-16 02:39:11 +0800

    增加电子病历数据类和电子病历智能合约

---
+++ src/main/java/org/ray/MedicalRecordContract.java
@@ -0,0 +1,12 @@
+package org.ray;
+
+
+import lombok.Data;
+import org.hyperledger.fabric.contract.annotation.Contract;
+import org.hyperledger.fabric.contract.annotation.DataType;
+
+@Contract(
+        name = "MedicalRecordContract"
+)
+public class MedicalRecordContract {
+}
